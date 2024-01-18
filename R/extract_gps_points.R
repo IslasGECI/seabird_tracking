560600621b73a49a21654a69d85a2a03f54920d1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-18 13:11:14 -0800

    ♻️  Refactor

--- R/extract_gps_points.R
+++ R/extract_gps_points.R
@@ -8,9 +8,11 @@ clean_gps_from_txt <- function(raw_path, bird_id, output_path) {
 }
 
 extract_points <- function(raw_path) {
+  temporal_file <- "tmp.txt"
+  command <- glue::glue("cat {raw_path} | grep --invert-match 'EVENT' > {temporal_file}")
+  system(command)
   columns_of_interest <- c("Date", "Time", "Longitude", "Latitude")
-  raw <- readr::read_tsv(raw_path, skip = 15, col_names = columns_of_interest, show_col_types = FALSE) |>
+  raw <- readr::read_tsv(temporal_file, col_names = columns_of_interest, show_col_types = FALSE) |>
     dplyr::select(dplyr::all_of(columns_of_interest))
-  clean_rows <- subset(raw, !stringr::str_detect(Date, "^EVENT"))
-  return(clean_rows)
+  return(raw)
 }
