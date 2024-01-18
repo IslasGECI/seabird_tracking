0c24da2c522f673c74a0fba2337305f870381192
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-18 10:14:25 -0800

    ♻️  Refactor

--- R/extract_gps_points.R
+++ R/extract_gps_points.R
@@ -1,7 +1,7 @@
 extract_points <- function(raw_path) {
   columns_of_interest <- c("Date", "Time", "Longitude", "Latitude")
   raw <- readr::read_tsv(raw_path, skip = 15, col_names = columns_of_interest, show_col_types = FALSE) |>
-    dplyr::select(columns_of_interest)
+    dplyr::select(dplyr::all_of(columns_of_interest))
   clean_rows <- subset(raw, !stringr::str_detect(Date, "^EVENT"))
   return(clean_rows)
 }
