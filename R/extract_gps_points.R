f308d30c27d9d1ed48d6c518d6284b655a49252e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-18 10:12:15 -0800

    ✅ Pass tests

--- R/extract_gps_points.R
+++ R/extract_gps_points.R
@@ -1,4 +1,6 @@
 extract_points <- function(raw_path) {
-  readr::read_tsv(raw_path, skip = 15, col_names = c("Date", "Time", "Longitude", "Latitude")) |>
+  raw <- readr::read_tsv(raw_path, skip = 15, col_names = c("Date", "Time", "Longitude", "Latitude")) |>
     dplyr::select(c("Date", "Time", "Longitude", "Latitude"))
+  clean_rows <- subset(raw, !stringr::str_detect(Date, "^EVENT"))
+  return(clean_rows)
 }
