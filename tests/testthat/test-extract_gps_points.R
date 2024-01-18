40ad018c135f5916fc056fad4b8768c27ce506be
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-18 10:03:23 -0800

    🛑🧪 Fail tests

--- tests/testthat/test-extract_gps_points.R
+++ tests/testthat/test-extract_gps_points.R
@@ -2,8 +2,12 @@ describe("extract gps for each file", {
   it("read tsv", {
     raw_path <- "/workdir/tests/data/raw_gps_albatros.txt"
     obtained_points <- extract_points(raw_path)
-    obtained_number_of_columns <- ncols(obtained_points)
+    obtained_number_of_columns <- ncol(obtained_points)
     expected_number_of_columns <- 4
     expect_equal(obtained_number_of_columns, expected_number_of_columns)
+
+    obtained_number_of_rows <- nrow(obtained_points)
+    expected_number_of_rows <- 15
+    expect_equal(obtained_number_of_rows, expected_number_of_rows)
   })
 })
