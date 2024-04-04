70dec96048cfc2a0178d6fdf7501571fe6a195ae
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-04 11:20:51 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-extract_gps_points.R
+++ tests/testthat/test-extract_gps_points.R
@@ -26,6 +26,11 @@ describe("extract gps for each file", {
 
 describe("extract gps from i-gatU device", {
   raw_path <- "/workdir/tests/data/raw_gps_albatros_igatu.csv"
+  it("clean csv for each bird id", {
+    bird_id <- "bird_1"
+    output_path <- "/workdir/tests/data/cleaned_gps_from_igatu.csv"
+    clean_gps_from_csv(raw_path, bird_id, output_path)
+  })
   it("read csv", {
     obtained_points <- extract_points_from_csv(raw_path)
     obtained_columns <- colnames(obtained_points)
