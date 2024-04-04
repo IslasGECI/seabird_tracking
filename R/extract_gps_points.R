6bb6ddab942c0e612a2cd4ede438eb23e1e8c49d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-04 11:32:29 -0700

    ♻️  Refactor

--- R/extract_gps_points.R
+++ R/extract_gps_points.R
@@ -2,8 +2,7 @@
 clean_gps_from_txt <- function(raw_path, bird_id, output_path) {
   extracted_points <- extract_points_from_txt(raw_path)
   points_with_id <- extracted_points |>
-    dplyr::mutate(bird_id = bird_id) |>
-    dplyr::mutate(Date = lubridate::dmy(Date))
+    dplyr::mutate(bird_id = bird_id)
   readr::write_csv(points_with_id, output_path)
 }
 
@@ -20,7 +19,8 @@ extract_points_from_txt <- function(raw_path) {
   system(command)
   columns_of_interest <- c("Date", "Time", "Longitude", "Latitude")
   raw <- readr::read_tsv(temporal_file, col_names = columns_of_interest, show_col_types = FALSE) |>
-    dplyr::select(dplyr::all_of(columns_of_interest))
+    dplyr::select(dplyr::all_of(columns_of_interest)) |>
+    dplyr::mutate(Date = lubridate::dmy(Date))
   return(raw)
 }
 
