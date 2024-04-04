c3a6ac2084245ae16e527108450fd99b7db20aa0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-04 12:51:46 -0700

    ♻️  Refactor

--- R/extract_gps_points.R
+++ R/extract_gps_points.R
@@ -23,6 +23,10 @@ xxclean_gps_from_txt <- function(raw_path, bird_id) {
 
 #' @export
 clean_gps_from_csv <- function(raw_path, bird_id, output_path) {
+  xxclean_gps_from_csv(raw_path, bird_id, output_path)
+}
+
+xxclean_gps_from_csv <- function(raw_path, bird_id, output_path) {
   extracted_points <- extract_points_from_csv(raw_path)
   points_with_id <- add_bird_id(extracted_points, bird_id)
   readr::write_csv(points_with_id, output_path)
