d8df01e94ad55733e3d4d4999bdc7449990c3f16
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-04 11:58:42 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-extract_gps_points.R
+++ tests/testthat/test-extract_gps_points.R
@@ -1,8 +1,19 @@
 describe("extract gps for each file", {
-  raw_path <- "/workdir/tests/data/raw_gps_albatros.txt"
-  it("clean gps", {
+  it("clean gps from txt", {
+    raw_path <- "/workdir/tests/data/raw_gps_albatros.txt"
     bird_id <- "bird_1"
     clean_gps(raw_path, bird_id)
+
+    output_path <- "/workdir/tests/data/bird_1.csv"
+    obtained <- read_csv(output_path, show_col_types = FALSE)
+
+    obtained_id <- obtained$bird_id[[1]]
+    expect_equal(obtained_id, bird_id)
+    obtained_date <- obtained$Date[[1]]
+    expected_date <- as.Date("2014-01-25")
+    expect_equal(obtained_date, expected_date)
+    expect_true(testtools::exist_output_file(output_path))
+    testtools::delete_output_file(output_path)
   })
 })
 describe("extract gps technosmart", {
