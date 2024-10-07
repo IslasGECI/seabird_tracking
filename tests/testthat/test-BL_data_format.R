2e2608c320e255e39a7a806714e7968932b394af
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-07 14:03:11 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-BL_data_format.R
+++ tests/testthat/test-BL_data_format.R
@@ -45,6 +45,7 @@ describe("Construct BL table", {
 describe("Join data columns", {
   breeding_status <- read_csv(breeding_status_path, show_col_types = FALSE)
   tracking_data <- read_csv(tracking_path, show_col_types = FALSE)
+  print(tracking_data)
   obtained <- join_seabird_breeding_status_with_tracking_data(breeding_status, tracking_data)
   it("Check columns", {
     expected_columns <- c(
@@ -54,6 +55,7 @@ describe("Join data columns", {
       "sex",
       "lat_colony",
       "lon_colony",
+      "season",
       "breed_stage",
       "breed_status",
       "date_gmt",
