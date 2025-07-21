f77c04bba881a609ecf40d01144231622d7bddb5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-21 14:33:09 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-BL_data_format.R
+++ tests/testthat/test-BL_data_format.R
@@ -75,7 +75,8 @@ describe("Classify breed stage from hatching and brood dates", {
   )
   it("Classify incubation", {
     obtained <- classify_breed_stage(data)
-    obtained_breed_stage <- obtained[[1, "breed_stage"]]
+    print(obtained)
+    obtained_breed_stage <- obtained[1, "breed_stage"]
     expect_equal(obtained_breed_stage, "incubation")
   })
 })
