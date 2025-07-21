0e67572a00772e0f529a72d5b7ebd9e5fabf4c69
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-21 14:55:43 -0700

    ✅ Pass tests

--- R/BL_data_format.R
+++ R/BL_data_format.R
@@ -33,7 +33,7 @@ classify_breed_stage <- function(data) {
   data |>
     mutate(breed_stage = case_when(
       date_gmt < hatching_end_date ~ "incubation",
-      date_gmt < brood_end_date & date_gmt > hatching_end_date ~ "brood-guard",
+      date_gmt < brood_end_date & date_gmt >= hatching_end_date ~ "brood-guard",
       date_gmt > brood_end_date ~ "chick-rearing"
     ))
 }
