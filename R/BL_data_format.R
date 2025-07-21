c8c5ff5b9671767665655f49b9a729e9d8fca286
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-21 14:50:18 -0700

    ✅ Pass tests

--- R/BL_data_format.R
+++ R/BL_data_format.R
@@ -32,7 +32,11 @@ join_seabird_breeding_status_with_tracking_data <- function(breeding_status, tra
 classify_breed_stage <- function(data) {
   data |>
     mutate(hatching_end_date = lubridate::ymd(hatching_end_date), date_gmt = lubridate::ymd(date_gmt)) |>
-    mutate(breed_stage = case_when(date_gmt < hatching_end_date ~ "incubation", date_gmt > hatching_end_date ~ "brood-guard"))
+    mutate(breed_stage = case_when(
+      date_gmt < hatching_end_date ~ "incubation",
+      date_gmt < brood_end_date & date_gmt > hatching_end_date ~ "brood-guard",
+      date_gmt > brood_end_date ~ "chick-rearing"
+    ))
 }
 
 get_metadata <- function(datapackage_path, resource_name = "breeding_status_albatross_guadalupe") {
