9ada653d960c95a49a90135317f6d41396a97780
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-21 14:19:38 -0700

    ♻️  Refactor

--- tests/testthat/test-BL_data_format.R
+++ tests/testthat/test-BL_data_format.R
@@ -5,18 +5,6 @@ datapackage_path <- "../data/datapackage.json"
 breeding_status_path <- "../data/breeding_status_albatross_guadalupe.csv"
 tracking_path <- "../data/gps-albatros-guadalupe.csv"
 
-describe("write_bl_table", {
-  it("construct_bl_table ", {
-    relative_path <- paste0(getwd(), "/../data/")
-    setwd(relative_path)
-    output_path <- "bl_albatross_guadalupe.csv"
-    datapackage_path <- "datapackage.json"
-    testtools::if_exist_remove(output_path)
-    write_bl_table(datapackage_path, output_path)
-    expect_true(testtools::exist_output_file(output_path))
-  })
-})
-
 describe("Construct BL table", {
   it("construct_bl_table ", {
     obtained_bl_table <- construct_bl_table(breeding_status_path, tracking_path, datapackage_path)
