ea96ab33d8cddafe98d33a55cafc8659f40d82ff
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-22 13:30:19 -0700

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -7,6 +7,16 @@ write_bl_table <- function(options) {
   write_csv(bl_table, options[["output-path"]])
 }
 
+#' @export
+get_domain_specific_options <- function() {
+  breeding_status_path <- geci.optparse::character_option(c("-b", "--breeding-status"), default = "/workdir/breeding_status.csv", help = "File path of the breeding_status_database")
+  tracking_path <- geci.optparse::character_option(c("-c", "--tracking-data"), default = "/workdir/gps-albatros-guadalupe.csv", help = "File path of the tracking database")
+  config_path <- geci.optparse::character_option(c("-c", "--config-path"), default = "/workdir/reports/non-tabular/config_file.json", help = "File path of the configuration")
+  output_path <- geci.optparse::character_option(c("-o", "--output-path"), default = "/workdir/reports/tables/result.csv", help = "File path of the desire output")
+  option_names <- c(breeding_status_path, tracking_path, config_path, output_path)
+  geci.optparse::get_options_from_vec(option_names)
+}
+
 read_config <- function(config_path) {
   json_content <- rjson::fromJSON(file = config_path)
   json_content$colony <- tibble::tibble(Longitude = json_content$lon_colony, Latitude = json_content$lat_colony)
