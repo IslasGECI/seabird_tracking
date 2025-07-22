f9d4da635519efea55f5083b3e2a16ad9d1bf263
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-22 13:14:07 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,9 +1,7 @@
 #' @export
-write_bl_table <- function(datapackage_path = "datapackage.json", output_path = "bl_gps_albatross_guadalupe.csv") {
-  breeding_status_path <- get_metadata_path(datapackage_path, "breeding_status_albatross_guadalupe")
-  tracking_path <- get_metadata_path(datapackage_path, "gps-albatros-guadalupe")
-  breeding_status <- read_csv(breeding_status_path, show_col_types = FALSE)
-  tracking_data <- read_csv(tracking_path, show_col_types = FALSE)
+write_bl_table <- function(options) {
+  breeding_status <- read_csv(options[["breeding-status"]], show_col_types = FALSE)
+  tracking_data <- read_csv(options[["tracking-data"]], show_col_types = FALSE)
   bl_table <- construct_bl_table(breeding_status, tracking_data)
-  write_csv(bl_table, output_path)
+  write_csv(bl_table, options[["output-path"]])
 }
