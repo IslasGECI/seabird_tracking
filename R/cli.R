6da380486927b073a0496c80c48fe68b412418e1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-22 13:19:35 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -2,8 +2,13 @@
 write_bl_table <- function(options) {
   breeding_status <- read_csv(options[["breeding-status"]], show_col_types = FALSE)
   tracking_data <- read_csv(options[["tracking-data"]], show_col_types = FALSE)
-  colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
-  config_content <- list(inner_buff = 60, return_buff = 60, duration = 1, colony = colony_df)
+  config_content <- read_config(options[["config-path"]])
   bl_table <- construct_bl_table(breeding_status, tracking_data, config_content)
   write_csv(bl_table, options[["output-path"]])
+}
+
+read_config <- function(config_path) {
+  json_content <- rjson::fromJSON(file = config_path)
+  json_content$colony <- tibble::tibble(Longitude = json_content$lon_colony, Latitude = json_content$lat_colony)
+  return(json_content)
 }
