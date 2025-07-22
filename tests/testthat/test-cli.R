26cb8f3f98644ad81c2d6e0b0640a9b37352b05f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-22 13:11:36 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-cli.R
+++ tests/testthat/test-cli.R
@@ -1,11 +1,15 @@
 describe("write_bl_table", {
   it("construct_bl_table ", {
-    relative_path <- "/workdir/tests/data"
-    setwd(relative_path)
-    output_path <- "bl_albatross_guadalupe.csv"
-    datapackage_path <- "datapackage.json"
+    output_path <- "/workdir/tests/data/bl_albatross_guadalupe.csv"
+    config_path <- "/workdir/tests/data/trips_config.json"
     testtools::if_exist_remove(output_path)
-    write_bl_table(datapackage_path, output_path)
+    options_list <- list(
+      "config-path" = config_path,
+      "breeding-status" = "/workdir/tests/data/breeding_status_albatross_guadalupe.csv",
+      "tracking-data" = "/workdir/tests/data/gps-albatros-guadalupe.csv",
+      "output-path" = output_path
+    )
+    write_bl_table(options_list)
     expect_true(testtools::exist_output_file(output_path))
   })
 })
