7bc5b711d2b39f563e4f71aa6c13936e7d18ef30
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-03 18:40:26 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-radar_signal.R
+++ tests/testthat/test-radar_signal.R
@@ -19,7 +19,7 @@ describe("Assign coordinates to radar signal data", {
     expect_true(all(expected_columns %in% obtained_columns))
     expect_equal(nrow(obtained), 1)
   })
-  it("with match on id and date time", {
+  it("without match on id and date time should return no rows", {
     tracking_data <- tibble::tibble(
       name = c("4E8", "4E8", "4H2"),
       date = as.Date(c("2020-01-01", "2020-01-02", "2020-01-01")),
@@ -35,6 +35,24 @@ describe("Assign coordinates to radar signal data", {
     )
     obtained <- assign_coordinates_to_radar_signal(radar_signal, tracking_data)
     expected_rows <- 0
+    expect_equal(nrow(obtained), expected_rows)
+  })
+  it("Fill tracking data with missing records in radar signal data", {
+    tracking_data <- tibble::tibble(
+      name = c("4E8", "4E8", "4H2"),
+      date = as.Date(c("2020-01-01", "2020-01-02", "2020-01-01")),
+      time = c("12:00:00", "12:00:00", "12:00:00"),
+      latitude = c(10, 20, 15),
+      longitude = c(30, 40, 100)
+    )
+    radar_signal <- tibble::tibble(
+      bird_id = "4E8",
+      date = as.Date("2020-01-01"),
+      time = "15:00:00",
+      radar_signal = 3
+    )
+    obtained <- fill_tracking_data_with_dates_on_radar_signal(radar_signal, tracking_data)
+    expected_rows <- 4
     expect_equal(nrow(obtained), expected_rows)
   })
 })
