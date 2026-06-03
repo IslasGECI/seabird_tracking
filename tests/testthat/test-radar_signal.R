713ab2142ff1f168cf43f3bc265b221a5e361b3d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-03 21:57:43 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-radar_signal.R
+++ tests/testthat/test-radar_signal.R
@@ -1,11 +1,11 @@
 describe("Assign coordinates to radar signal data", {
   it("Compute radar signal database", {
     tracking_data <- tibble::tibble(
-      name = c("4E8", "4E8", "4H2"),
-      date = as.Date(c("2020-01-01", "2020-01-02", "2020-01-01")),
-      time = c("12:00:00", "12:00:00", "12:00:00"),
-      latitude = c(10, 20, 15),
-      longitude = c(30, 40, 100)
+      name = c("4E8", "4E8", "4H2", "4H2"),
+      date = as.Date(c("2020-01-01", "2020-01-02", "2020-01-01", "2020-01-02")),
+      time = c("12:00:00", "12:00:00", "12:00:00", "12:00:00"),
+      latitude = c(10, 20, 15, 50),
+      longitude = c(30, 40, 100, 110)
     )
     radar_signal <- tibble::tibble(
       bird_id = c("4E8", "1K0"),
