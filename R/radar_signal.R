2e0759283778a2a7fb8e26d15a9ac68876acdb7e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-03 20:24:38 +0000

    ✅ Pass tests

--- R/radar_signal.R
+++ R/radar_signal.R
@@ -6,5 +6,20 @@ assign_coordinates_to_radar_signal <- function(radar_signal, tracking_data) {
 }
 
 fill_tracking_data_with_dates_on_radar_signal <- function(radar_signal, tracking_data) {
-  dplyr::bind_rows(tracking_data, radar_signal |> dplyr::rename(name = bird_id))
+  tracking_data_with_datetime <- tracking_data |>
+    dplyr::mutate(datetime = lubridate::ymd_hms(paste0(date, " ", time)))
+
+  radar_signal_with_datetime <- radar_signal |>
+    dplyr::mutate(datetime = lubridate::ymd_hms(paste0(date, " ", time)))
+
+  tracking_data_range_by_id <- tracking_data_with_datetime |>
+    dplyr::group_by(name) |>
+    dplyr::summarize(min_datetime = min(datetime), max_datetime = max(datetime))
+
+  filtered_radar_signal <- radar_signal_with_datetime |>
+    dplyr::inner_join(tracking_data_range_by_id, by = dplyr::join_by(bird_id == name)) |>
+    dplyr::filter(datetime >= min_datetime & datetime <= max_datetime) |>
+    dplyr::transmute(name = bird_id, date, time, latitude = NA_real_, longitude = NA_real_)
+
+  dplyr::bind_rows(tracking_data, filtered_radar_signal)
 }
