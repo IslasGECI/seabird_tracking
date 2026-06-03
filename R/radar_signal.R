2f056e4f7071cad72b59fb1c4a7115c3bb5ac99a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-03 18:27:43 +0000

    ✅ Pass tests

--- R/radar_signal.R
+++ R/radar_signal.R
@@ -2,5 +2,5 @@ assign_coordinates_to_radar_signal <- function(radar_signal, tracking_data) {
   radar_signal_with_coordinates <- radar_signal |>
     dplyr::left_join(tracking_data, by = dplyr::join_by(bird_id == name, date, time)) |>
     dplyr::rename(name = bird_id)
-  radar_signal_with_coordinates
+  radar_signal_with_coordinates |> drop_na(latitude, longitude)
 }
