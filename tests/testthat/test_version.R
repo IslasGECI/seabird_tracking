5f5f83e5f245ce7fa20f99165367b0a8aebe29c0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-04 17:27:38 +0000

    ♻️ Refactor: 🆙update minor number version

--- tests/testthat/test_version.R
+++ tests/testthat/test_version.R
@@ -1,6 +1,6 @@
 describe("Get version of the module", {
-  it("The version is 1.2.0", {
-    expected_version <- c("1.2.0")
+  it("The version is 1.3.0", {
+    expected_version <- c("1.3.0")
     obtained_version <- packageVersion("seabirdtracking")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
